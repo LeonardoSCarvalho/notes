a8bf65ee7eb32b78f10813abf7ecfbf9345bede6
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-12 00:02:20 -0300

    feat: create valid note

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,13 +1,13 @@
-import { Either, left, right } from "@/shared/either"
 import { InvalidEmailError } from "@/entities/errors/invalid-email-error"
+import { Either, left, right } from "@/shared/either"
 
 export class Email {
   private constructor(public readonly email: string) {}
+
   public static create(email: string): Either<InvalidEmailError, Email> {
     if (Email.validate(email)) {
       return right(new Email(email))
     }
-
     return left(new InvalidEmailError())
   }
 
@@ -15,27 +15,21 @@ export class Email {
     if (!email) {
       return false
     }
-
     if (email.length > 320) {
       return false
     }
-
     const emailRegex =
       /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
-
     if (!emailRegex.test(email)) {
       return false
     }
-
     const [local, domain] = email.split("@")
     if (local.length > 64 || local.length === 0) {
       return false
     }
-
     if (domain.length > 255 || domain.length === 0) {
       return false
     }
-
     const domainParts = domain.split(".")
     if (
       domainParts.some(function (part) {
@@ -44,7 +38,6 @@ export class Email {
     ) {
       return false
     }
-
     return true
   }
 }
---
+++ src/entities/note.ts
@@ -0,0 +1,24 @@
+import { Either, left, right } from "@/shared/either"
+import { InvalidTitleError } from "./errors/invalid-title-error"
+import { Title } from "./title"
+import { User } from "./user"
+
+export class Note {
+  constructor(private readonly _owner: User, private readonly _title: Title) {}
+
+  get title(): Title {
+    return this._title
+  }
+  get owner(): User {
+    return this._owner
+  }
+
+  public static create(
+    owner: User,
+    title: string
+  ): Either<InvalidTitleError, Note> {
+    const titleOrError = Title.create(title)
+    if (titleOrError.isLeft()) return left(titleOrError.value)
+    return right(new Note(owner, titleOrError.value))
+  }
+}
