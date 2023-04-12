6f40416ff5b3d6569fb670ee4dcfa11d4f74b8fd
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-11 23:57:04 -0300

    feat: create valid title

---
+++ src/entities/title.ts
@@ -0,0 +1,18 @@
+import { Either, left, right } from "@/shared/either"
+import { InvalidTitleError } from "./errors/invalid-title-error"
+
+export class Title {
+  private constructor(public readonly value: string) {}
+
+  public static create(title: string): Either<InvalidTitleError, Title> {
+    if (Title.validate(title)) {
+      return right(new Title(title))
+    }
+    return left(new InvalidTitleError())
+  }
+  public static validate(title: string): boolean {
+    if (!title) return false
+    if (title.trim().length < 3 || title.trim().length > 256) return false
+    return true
+  }
+}
