bf93b454b56f94ef631f012cf07dc87405197726
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-12 22:56:23 -0300

    feat: implement password on user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,14 +1,22 @@
 import { Either, left, right } from "@/shared/either"
 import { Email } from "./email"
 import { InvalidEmailError } from "./errors/invalid-email-error"
+import { InvalidPasswordError } from "./errors/invalid-password-error"
+import { Password } from "./password"
 import { UserData } from "./user-data"
 
 export class User {
   private constructor(public readonly email: Email) {}
-  public static create(userData: UserData): Either<InvalidEmailError, User> {
+  public static create(
+    userData: UserData
+  ): Either<InvalidEmailError | InvalidPasswordError, User> {
     const emailOrError = Email.create(userData.email)
+    const passwordOrError = Password.create(userData.password)
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
+    }
+    if (passwordOrError.isLeft()) {
+      return left(new InvalidPasswordError())
     }
     const email = emailOrError.value as Email
     return right(new User(email))
