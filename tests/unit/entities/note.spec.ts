5076723447a974f81ba16e70b7b9140ac9c426d1
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-12 22:56:42 -0300

    test: implement password on user

--- tests/unit/entities/note.spec.ts
+++ tests/unit/entities/note.spec.ts
@@ -3,8 +3,10 @@ import { User } from "@/entities/user"
 
 describe("Note entity", () => {
   it("Should be created if title and owner are valid", () => {
-    const validOwner: User = User.create({ email: "any@email.com" })
-      .value as User
+    const validOwner: User = User.create({
+      email: "any@email.com",
+      password: "any-password",
+    }).value as User
     const validTitle = "any title"
     const note = Note.create(validOwner, validTitle).value as Note
     expect(note.title.value).toEqual(validTitle)
--- tests/unit/entities/user.spec.ts
+++ tests/unit/entities/user.spec.ts
@@ -1,16 +1,37 @@
 import { InvalidEmailError } from "@/entities/errors/invalid-email-error"
+import { InvalidPasswordError } from "@/entities/errors/invalid-password-error"
 import { User } from "@/entities/user"
 import { left } from "@/shared/either"
 
 describe("User domain entity", () => {
   it("Should not create user with invalid e-mail address", () => {
     const invalidEmail = "invalid_email"
-    const error = User.create({ email: invalidEmail })
+    const validPassword = "123any-password"
+    const error = User.create({ email: invalidEmail, password: validPassword })
     expect(error).toEqual(left(new InvalidEmailError()))
   })
-  it("Should create user with valid email address", () => {
+
+  it("Should not create user with invalid password (less than 6 characters)", () => {
+    const validEmail = "any@email.com"
+    const invalidPassword = "123ac"
+    const error = User.create({ email: validEmail, password: invalidPassword })
+    expect(error).toEqual(left(new InvalidPasswordError()))
+  })
+
+  it("Should not create user with invalid password (without numbers)", () => {
+    const validEmail = "any@email.com"
+    const invalidPassword = "any-password"
+    const error = User.create({ email: validEmail, password: invalidPassword })
+    expect(error).toEqual(left(new InvalidPasswordError()))
+  })
+
+  it("Should create user with valid email address and password", () => {
     const validEmail = "any-email@email.com"
-    const user: User = User.create({ email: validEmail }).value as User
+    const validPassword = "123any-password"
+    const user: User = User.create({
+      email: validEmail,
+      password: validPassword,
+    }).value as User
     expect(user.email.value).toEqual(validEmail)
   })
 })
