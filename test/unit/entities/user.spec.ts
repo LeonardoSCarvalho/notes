6ec33ee87e79557711154202051ad41ada1b391c
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-13 23:08:08 -0300

    refactor: make frequently used makeSut

--- test/unit/entities/user.spec.ts
+++ test/unit/entities/user.spec.ts
@@ -3,32 +3,51 @@ import { InvalidPasswordError } from "@/entities/errors/invalid-password-error"
 import { User } from "@/entities/user"
 import { left } from "@/shared/either"
 
+const makeSut = () => {
+  const validEmail = "any-email@email.com"
+  const validPassword = "123any-password"
+  const invalidEmail = "invalid_email"
+  const invalidPasswordWithTooFewCharacters = "123ac"
+  const invalidPasswordWithNoNumbers = "invalid"
+  const sut = User
+  return {
+    sut,
+    validEmail,
+    validPassword,
+    invalidEmail,
+    invalidPasswordWithTooFewCharacters,
+    invalidPasswordWithNoNumbers,
+  }
+}
+
 describe("User domain entity", () => {
   it("Should not create user with invalid e-mail address", () => {
-    const invalidEmail = "invalid_email"
-    const validPassword = "123any-password"
-    const error = User.create({ email: invalidEmail, password: validPassword })
+    const { sut, invalidEmail, validPassword } = makeSut()
+    const error = sut.create({ email: invalidEmail, password: validPassword })
     expect(error).toEqual(left(new InvalidEmailError()))
   })
 
   it("Should not create user with invalid password (less than 6 characters)", () => {
-    const validEmail = "any@email.com"
-    const invalidPassword = "123ac"
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const { sut, validEmail, invalidPasswordWithTooFewCharacters } = makeSut()
+    const error = sut.create({
+      email: validEmail,
+      password: invalidPasswordWithTooFewCharacters,
+    })
     expect(error).toEqual(left(new InvalidPasswordError()))
   })
 
   it("Should not create user with invalid password (without numbers)", () => {
-    const validEmail = "any@email.com"
-    const invalidPassword = "any-password"
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const { sut, validEmail, invalidPasswordWithNoNumbers } = makeSut()
+    const error = sut.create({
+      email: validEmail,
+      password: invalidPasswordWithNoNumbers,
+    })
     expect(error).toEqual(left(new InvalidPasswordError()))
   })
 
   it("Should create user with valid email address and password", () => {
-    const validEmail = "any-email@email.com"
-    const validPassword = "123any-password"
-    const user: User = User.create({
+    const { sut, validEmail, validPassword } = makeSut()
+    const user: User = sut.create({
       email: validEmail,
       password: validPassword,
     }).value as User
