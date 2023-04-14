e47fcc4af6944e81b39134332711d600c1591436
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-13 23:27:39 -0300

    test: prevent signup to register existing user

--- test/unit/application/use-cases/signup.spec.ts
+++ test/unit/application/use-cases/signup.spec.ts
@@ -1,6 +1,7 @@
 import { Encoder } from "@/application/ports/encoder"
 import { UserRepository } from "@/application/ports/user-repository"
 import { Signup } from "@/application/use-cases/signup"
+import { ExistingUserError } from "@/entities/errors/existing-user-error"
 import { UserData } from "@/entities/user-data"
 import { FakeEncoder } from "../repositories/fake-encoder"
 import { InMemoryUserRepository } from "../repositories/in-memory-user-repository"
@@ -15,13 +16,27 @@ describe("Signup use case", () => {
     const userRepository: UserRepository = new InMemoryUserRepository([])
     const encoder: Encoder = new FakeEncoder()
     const usecase: Signup = new Signup(userRepository, encoder)
-    const userSignupResponse: UserData = await usecase.perform(
-      userSignupRequest
-    )
-    expect(userSignupResponse).toEqual(userSignupRequest)
+    const userSignupResponse = await usecase.perform(userSignupRequest)
+    expect(userSignupResponse.value).toEqual(userSignupRequest)
     expect((await userRepository.findAllUsers()).length).toEqual(1)
     expect(
       (await userRepository.findUserByEmail(validEmail))?.password
     ).toEqual(validPassword + "ENCRYPTED")
+  })
+  it("Should not signup if user already exists", async () => {
+    const validEmail = "any@email.com"
+    const validPassword = "123any_password"
+    const userSignupRequest: UserData = {
+      email: validEmail,
+      password: validPassword,
+    }
+    const userRepository: UserRepository = new InMemoryUserRepository([
+      { ...userSignupRequest },
+    ])
+    const encoder: Encoder = new FakeEncoder()
+    const usecase: Signup = new Signup(userRepository, encoder)
+    const userSignupResponse = await usecase.perform(userSignupRequest)
+    expect(userSignupResponse.isLeft()).toBeTruthy()
+    expect(userSignupResponse.value).toEqual(new ExistingUserError())
   })
 })
