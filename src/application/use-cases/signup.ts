f270ce6c5d05795723bf26e0048209cfd37d9b55
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-13 23:00:27 -0300

    feat: initial signup use case implementaion

---
+++ src/application/use-cases/signup.ts
@@ -0,0 +1,19 @@
+import { UserData } from "@/entities/user-data"
+import { Encoder } from "../ports/encoder"
+import { UserRepository } from "../ports/user-repository"
+
+export class Signup {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly encoder: Encoder
+  ) {}
+
+  public async perform(userSignupRequest: UserData): Promise<UserData> {
+    const encodedPassword = this.encoder.encode(userSignupRequest.password)
+    this.userRepository.addUser({
+      ...userSignupRequest,
+      password: encodedPassword,
+    })
+    return userSignupRequest
+  }
+}
