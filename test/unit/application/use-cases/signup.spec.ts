283fa10c232ae99ad5a5fffd16c2a0a5d33f2fda
leonardo carvalho <leo.carvalholkp@gmail.com>
2023-04-25 22:57:42 -0300

    refactor: use a user builder to make tests self-contained

--- test/unit/application/use-cases/signup.spec.ts
+++ test/unit/application/use-cases/signup.spec.ts
@@ -7,65 +7,54 @@ import { InvalidPasswordError } from "@/entities/errors/invalid-password-error"
 import { UserData } from "@/entities/user-data"
 import { FakeEncoder } from "../repositories/fake-encoder"
 import { InMemoryUserRepository } from "../repositories/in-memory-user-repository"
+import { UserBuilder } from "./builders/user-builder"
 describe("Signup use case", () => {
   it("Should signup user with valid data", async () => {
-    const validEmail = "any@mail.com"
-    const validPassword = "123any_password"
-    const userSignupRequest: UserData = {
-      email: validEmail,
-      password: validPassword,
-    }
     const userRepository: UserRepository = new InMemoryUserRepository([])
     const encoder: Encoder = new FakeEncoder()
     const usecase: Signup = new Signup(userRepository, encoder)
-    const userSignupResponse = await usecase.perform(userSignupRequest)
+    const validUserSignUpRequest = UserBuilder.aUser().build()
+    const userSignupResponse = await usecase.perform(validUserSignUpRequest)
     expect(userSignupResponse.isRight()).toBeTruthy()
     expect((await userRepository.findAllUsers()).length).toEqual(1)
-    expect((await userRepository.findByEmail(validEmail))?.password).toEqual(
-      validPassword + "ENCRYPTED"
-    )
+    expect(
+      (await userRepository.findByEmail(validUserSignUpRequest.email))?.password
+    ).toEqual(validUserSignUpRequest.password + "ENCRYPTED")
   })
   it("Should not signup if user already exists", async () => {
-    const validEmail = "any@email.com"
-    const validPassword = "123any_password"
-    const userSignupRequest: UserData = {
-      email: validEmail,
-      password: validPassword,
-    }
+    const validUserSignUpRequest: UserData = UserBuilder.aUser().build()
     const userRepository: UserRepository = new InMemoryUserRepository([
-      { ...userSignupRequest },
+      { ...validUserSignUpRequest },
     ])
     const encoder: Encoder = new FakeEncoder()
     const usecase: Signup = new Signup(userRepository, encoder)
-    const userSignupResponse = await usecase.perform(userSignupRequest)
+    const userSignupResponse = await usecase.perform(validUserSignUpRequest)
     expect(userSignupResponse.isLeft()).toBeTruthy()
     expect(userSignupResponse.value).toEqual(new ExistingUserError())
   })
   it("Should not signup if email is invalid", async () => {
-    const invalidEmail = "invalid_email"
-    const validPassword = "123any_password"
-    const userSignupRequest: UserData = {
-      email: invalidEmail,
-      password: validPassword,
-    }
+    const userSignUpRequestWithInvalidEmail: UserData = UserBuilder.aUser()
+      .withInvalidEmail()
+      .build()
     const userRepository: UserRepository = new InMemoryUserRepository([])
     const encoder: Encoder = new FakeEncoder()
     const usecase: Signup = new Signup(userRepository, encoder)
-    const userSignupResponse = await usecase.perform(userSignupRequest)
+    const userSignupResponse = await usecase.perform(
+      userSignUpRequestWithInvalidEmail
+    )
     expect(userSignupResponse.isLeft()).toBeTruthy()
     expect(userSignupResponse.value).toEqual(new InvalidEmailError())
   })
   it("Should not signup if password is invalid", async () => {
-    const validEmail = "any@email.com"
-    const invalidPassword = "123"
-    const userSignupRequest: UserData = {
-      email: validEmail,
-      password: invalidPassword,
-    }
+    const userSignUpRequestWithInvalidPassword: UserData = UserBuilder.aUser()
+      .withInvalidPassword()
+      .build()
     const userRepository: UserRepository = new InMemoryUserRepository([])
     const encoder: Encoder = new FakeEncoder()
     const usecase: Signup = new Signup(userRepository, encoder)
-    const userSignupResponse = await usecase.perform(userSignupRequest)
+    const userSignupResponse = await usecase.perform(
+      userSignUpRequestWithInvalidPassword
+    )
     expect(userSignupResponse.isLeft()).toBeTruthy()
     expect(userSignupResponse.value).toEqual(new InvalidPasswordError())
   })
